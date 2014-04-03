a63456eb281d14d265ed2fe9bd36f1b53ba96317
denisstepanenko <denis.step88@gmail.com>
2014-04-03 21:46:45 +0100

    some work for adding friends to a user

---
+++ Chatter/API/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Chatter.API.DTOs;
+using Chatter.Data.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Chatter.API.Controllers
+{
+    public class UserController : ApiController
+    {
+        private ConceptualModelContainer db = new ConceptualModelContainer();
+        private int currentlyLoggedUserID = 1;//TODO: fix after authentication is done
+        
+        [HttpGet]
+        public IEnumerable<object> FindFriend(string searchCriteria)
+        {
+            var users = from u in db.Users
+                        let allowAddFriend = db.Friends.Where(f => f.UserID == currentlyLoggedUserID && f.FriendUserID == u.Id).Count() == 0
+                        where searchCriteria.Contains(u.FirstName) || searchCriteria.Contains(u.LastName) || searchCriteria.Contains(u.Email) || searchCriteria.Contains(u.Nickname)
+                        select new { name = u.FirstName + " " + u.LastName, allowAddFriend, id = u.Id };
+
+            return users;
+        }
+
+        [HttpPost]
+        public void AddToFriends(AddToFriendsDTO data)
+        {            
+            var friend = db.Friends.Where(f => f.FriendUserID == data.friendUserID && f.UserID == currentlyLoggedUserID).FirstOrDefault();
+            if (friend == null)
+            {
+                friend = new Friend();
+                friend.UserID = currentlyLoggedUserID;
+                friend.FriendUserID = data.friendUserID;
+
+                db.Friends.Add(friend);
+                db.SaveChanges();
+            }
+
+            //return "";
+        }
+
+    }
+}
---
+++ Chatter/Scripts/app/controllers/ctrl.chat.js
@@ -0,0 +1,22 @@
+﻿controllers.controller('ChatCtrl', ['$scope', '$http', '$log',
+	function ($scope, $http, $log) {
+	    $scope.friends = [];
+	    $scope.searchCriteria = "adsf";
+
+	    $scope.findFriend = function (searchCriteria) {
+	        //search for users matching the search criteria
+	        $http.get("/api/user/FindFriend", { params: { searchCriteria: searchCriteria } }).success(function (data) {
+	            $scope.friends = data;
+	        });
+	    }
+
+	    $scope.addToFriends = function (user) {
+	        $http.post("/api/user/addToFriends", { friendUserID: user.id }).success(function (data) {
+	            //repopulate friends list so that the "add friend" button is not shown anymore as the friend is already added
+	            $scope.findFriend($scope.searchCriteria);
+	        });
+
+	       
+	    }
+
+	}]);
