9d485d8d35c79251919a0fe7a6c22764ac1b4ac6
owenx1 <byrneo5@gmail.com>
2014-04-12 16:34:04 +0100

    added unit tests to chatter module. Also some packages were updated, so committing them as well.

--- Chatter/Scripts/tests/spec/ChatSpec.js
+++ Chatter/Scripts/tests/spec/ChatSpec.js
@@ -1,3 +1,6 @@
+//useful spy cheat sheet: http://tobyho.com/2011/12/15/jasmine-spy-cheatsheet/
+
+
 describe("ChatController", function () {
     beforeEach(module('ui.bootstrap'));
     beforeEach(module('controllers'));
@@ -9,13 +12,9 @@ describe("ChatController", function () {
     beforeEach(
         inject(function ($rootScope, $controller, _$httpBackend_, $http) {
             $scope = $rootScope.$new();
-            $scope.friendSearchCriteria = 'test';
-            $scope.friendsPaginationData = { totalItems: 1 };
+            
             $httpBackend = _$httpBackend_;
-
-            $httpBackend.when("GET", "/api/user/FindFriends?pageNumber=1&pageSize=10&searchCriteria=test")
-                .respond({ data: [{name: 'friend1'}, {name: 'friend2'}]});
-
+                        
             ctrl = $controller('ChatCtrl', {
                 $scope: $scope,
                 $http: $http,
@@ -25,9 +24,198 @@ describe("ChatController", function () {
     }));
 
     it("Get Friends", function () {
+        $scope.friendSearchCriteria = 'test';
+        $scope.friendsPaginationData = { currentPage:1, itemsPerPage:10 };
+
+        //searching friends
+        $httpBackend.expectGET("/api/user/FindFriends?pageNumber=1&pageSize=10&searchCriteria=test")
+            .respond({ data: [{ name: 'test1' }, { name: 'test2' }], totalItems: 2 });
+
         $scope.findFriends();
+
         $httpBackend.flush();
+
         expect($scope.friends.length).toEqual(2);
+
+        expect($scope.friendsPaginationData.totalItems).toEqual(2);
+    });
+
+    it("Get My Friends", function () {
+        $scope.myFriendsPaginationData = 'test';
+        $scope.myFriendsPaginationData = { currentPage: 1, itemsPerPage: 10 };
+
+        //searching my friends
+        $httpBackend.expectGET("/api/user/FindMyFriends?pageNumber=1&pageSize=10&searchCriteria=all")
+            .respond({ data: [{ name: 'test1' }, { name: 'test2' }, { name: 'test3' }], totalItems: 3 });
+
+        $scope.findMyFriends();
+                
+        $httpBackend.flush();
+
+        expect($scope.myFriends.length).toEqual(3);
+
+        expect($scope.myFriendsPaginationData.totalItems).toEqual(3);
+    });
+
+    it("Adds to friends and reload the fiends list", function () {
+        //adding to friends
+        $httpBackend.expectPOST("/api/user/addToFriends", { friendUserID: 1 })
+            .respond();
+
+        //searching friends
+        $httpBackend.expectGET("/api/user/FindFriends?pageNumber=1&pageSize=10")
+            .respond({ data: [{ name: 'test1' }, { name: 'test2' }], totalItems: 2 });
+               
+        $scope.addToFriends({ id: 1 });
+
+        $httpBackend.flush();       
+
+        expect($scope.friends.length).toEqual(2);
+
+        expect($scope.friendsPaginationData.totalItems).toEqual(2);
+    });
+
+    it("A friend is clicked and their chat history is retrieved", function () {
+        //searching friends
+        $httpBackend.expectGET("/api/user/getChatHistory?friendUserID=1&pageNumber=1&pageSize=10")
+            .respond({ data: [{ message: 'test1' }, { message: 'test2' }], totalItems: 2 });
+
+        var user = { id: 1, messageHistory: [] };
+        $scope.myFriendClick(user);
+
+        $httpBackend.flush();
+
+        expect($scope.activeFriend).toBe(user);
+        expect($scope.chatHistoryPaginationData.totalItems).toEqual(2);
+        expect($scope.activeFriend.messageHistory.length).toEqual(2);
+    });
+
+    it("Should not call a friend as they are currently talking that same friend", function () {
+        var friend = { id: 1 };
+        var serverObj = spyOn($.connection.chatHub.server, 'sendTextMessage');
+
+        $scope.activeCallUser = friend;
+
+        $scope.callFriend(friend);
+
+        expect(serverObj).not.toHaveBeenCalled();
+    });
+
+    it("Should not call a friend as they are currently talking to another friend and clicked cancel on confirmation message", function () {
+        var friend = { id: 1 };
+        var friend2 = { id: 2 };
+        var confirmSpy = spyOn(window, 'confirm').andReturn(false);
+
+        $scope.activeCallUser = friend;
+
+        $scope.callFriend(friend2);
+
+        expect(confirmSpy).toHaveBeenCalled();
+        expect($scope.activeCallUser).toBe(friend);
+    });
+
+    it("Should call a friend after ending the call with current friend", function () {
+        var friend = { id: 1 };
+        var friend2 = { id: 2 };
+        var confirmSpy = spyOn(window, 'confirm').andReturn(true);
+        var signalrSpy = spyOn($.connection.chatHub.server, "callRequest");
+        var endCallSpy = spyOn($scope, "endCall");
+        
+        $scope.activeCallUser = friend;
+
+        $scope.callFriend(friend2);
+
+        expect(confirmSpy).toHaveBeenCalled();
+        expect(signalrSpy).toHaveBeenCalled();
+        expect(endCallSpy).toHaveBeenCalled();
+        expect($scope.activeCallUser).toBe(friend2);
+    });
+
+    it("Should remove a friend from My Friends list (user didn't confirm)", function () {
+        var userToDelete = { id: 1 };
+        var userToBePresent={ id: 2 };
+
+        $scope.myFriends = [userToDelete, userToBePresent];
+        
+        expect($scope.myFriends.length).toEqual(2);
+
+        //when the user clicks cancel, nothing should be removed
+        spyOn(window, 'confirm').andReturn(false);
+        $scope.removeFriend(userToDelete);
+        expect($scope.myFriends.length).toEqual(2);                
+    });
+
+    it("Should remove a friend from My Friends list (confirmation failed)", function () {
+        var userToDelete = { id: 1 };
+        var userToBePresent = { id: 2 };
+
+        $scope.myFriends = [userToDelete, userToBePresent];
+
+        expect($scope.myFriends.length).toEqual(2);
+             
+        //when user confirms removal, only one item should remain after the AJAX call.
+        spyOn(window, 'confirm').andReturn(true);
+
+        //searching friends
+        $httpBackend.expectDELETE("/api/user/removeFriend?friendUserID=1")
+            .respond();
+
+        $scope.removeFriend(userToDelete);
+
+        $httpBackend.flush();
+
+        expect($scope.myFriends.length).toEqual(1);
+        expect($scope.myFriends.indexOf(userToBePresent)).toBe(0);
+    });
+    
+    it("Should not send a text message (user didn't enter the message)", function () {
+        var serverObj = spyOn($.connection.chatHub.server, 'sendTextMessage');
+        //var jqCall = spyOn(jQuery.fn, 'find');
+        
+        spyOn(jQuery.fn, 'val').andReturn("");
+
+        $scope.activeFriend = { messageHistory: [] };
+
+        $scope.sendTextMessage();
+
+        expect($scope.activeFriend.messageHistory.length).toEqual(0);
+        expect($scope.textMessage).toBe(undefined);//textbox wasn't reset
+        expect(serverObj).not.toHaveBeenCalled()
+    });
+
+    it("Should send a text message", function () {
+        var message = "msg";
+        
+        //var serverObj = jasmine.createSpyObj("$.connection.chatHub.server", ["sendTextMessage"]);
+        var serverObj = spyOn($.connection.chatHub.server, 'sendTextMessage');
+        var scrollSpy = spyOn($.fn, "scrollTop");
+
+        spyOn(jQuery.fn, 'val').andReturn(message);
+        
+        $scope.activeFriend = { id: 1, messageHistory: [] };
+        $scope.textMessage = message;
+
+        $httpBackend.expectPOST("/api/user/postTextMessage", {
+            friendUserID: 1,
+            message: message
+        }).respond();
+                      
+        $scope.sendTextMessage();
+
+        $httpBackend.flush();
+
+        expect(serverObj).toHaveBeenCalledWith($scope.activeFriend.id, message);
+        expect($scope.textMessage).toBe("");
+        expect(scrollSpy).toHaveBeenCalled();
+    });
+
+    it("Should trigger My Friends search in order to reload the list when My Friends tab is clicked", function () {
+        var spy = spyOn($scope, "findMyFriends");
+
+        $scope.friendsTabClick(1);
+
+        expect(spy).toHaveBeenCalled();
+        expect($scope.selectedTab).toEqual(1);
     });
 
 });
